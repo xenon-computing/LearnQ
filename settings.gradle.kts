631b744c8f65f4aea77319846e639fae7d118f86
xenon-computing <mdshadikulislam2007@gmail.com>
2026-05-14 14:54:31 +0600

    fix: explicit plugin versions declared in app module layout

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,22 @@
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/9.5.1/userguide/multi_project_builds.html in the Gradle documentation.
  */
 
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "LearnQ"
 include(":app")
+
